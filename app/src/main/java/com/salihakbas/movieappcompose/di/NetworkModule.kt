a7a1c3d67e0815eb4b114259afd628700465a291
Salih Akbas <salihhakbass@gmail.com>
2025-03-12 22:01:48 +0300

    Added tmdb api base url

--- app/src/main/java/com/salihakbas/movieappcompose/di/NetworkModule.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,10 +14,27 @@ import retrofit2.converter.gson.GsonConverterFactory
 object NetworkModule {
 
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("accept", "application/json")
+                    .addHeader(
+                        "Authorization",
+                        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2YxYTc2ODg2MjkxMzA2NmI4NjkwN2NmN2FiZGVmNiIsIm5iZiI6MTY5MzkxODE5Mi4wNiwic3ViIjoiNjRmNzIzZjA1ZjJiOGQwMGUxMmQ0OTkwIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.pQUZbWok_VuA6Ujq_0ZIpzoBng4yKjXPgKJoCNFAOoM"
+                    )
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://api.example.com/")
+            .baseUrl("https://api.themoviedb.org/3/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
