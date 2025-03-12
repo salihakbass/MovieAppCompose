f1f529814c19b304bcd4932415a871ba365d4d7f
Salih Akbas <salihhakbass@gmail.com>
2025-03-12 22:02:09 +0300

    Added getPopularMovies get function and usecase class

--- app/src/main/java/com/salihakbas/movieappcompose/data/source/remote/MainService.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/data/source/remote/MainService.kt
@@ -1,3 +1,14 @@
 package com.salihakbas.movieappcompose.data.source.remote
 
-interface MainService
+import com.salihakbas.movieappcompose.data.model.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MainService {
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1,
+        @Query("region") region: String? = null
+    ): MovieResponse
+}
---
+++ app/src/main/java/com/salihakbas/movieappcompose/domain/usecase/GetPopularMoviesUseCase.kt
@@ -0,0 +1,18 @@
+package com.salihakbas.movieappcompose.domain.usecase
+
+import com.salihakbas.movieappcompose.data.model.Movie
+import com.salihakbas.movieappcompose.data.source.remote.MainService
+import javax.inject.Inject
+
+class GetPopularMoviesUseCase @Inject constructor(
+    private val mainService: MainService
+) {
+    suspend operator fun invoke(language: String = "en-US", page: Int = 1): Result<List<Movie>> {
+        return try {
+            val movieResponse = mainService.getPopularMovies(language, page)
+            Result.success(movieResponse.results)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
