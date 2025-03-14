357169651840edd63ff29d61aecf18daf3a75eff
Salih Akbas <salihhakbass@gmail.com>
2025-03-15 02:04:33 +0300

    Added upcoming movies function

--- app/src/main/java/com/salihakbas/movieappcompose/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/data/repository/MainRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.salihakbas.movieappcompose.data.repository
 
+import com.salihakbas.movieappcompose.data.model.Movie
 import com.salihakbas.movieappcompose.data.source.local.MainDao
 import com.salihakbas.movieappcompose.data.source.remote.MainService
 import com.salihakbas.movieappcompose.domain.repository.MainRepository
@@ -8,4 +9,37 @@ import javax.inject.Inject
 class MainRepositoryImpl @Inject constructor(
     private val mainService: MainService,
     private val mainDao: MainDao,
-) : MainRepository
+) : MainRepository {
+    override suspend fun getPopularMovies(
+        language: String,
+        page: Int,
+        region: String?
+    ): List<Movie> {
+        return mainService.getPopularMovies(language, page, region).results
+    }
+
+    override suspend fun getNowPlayingMovies(
+        language: String,
+        page: Int,
+        region: String?
+    ): List<Movie> {
+        return mainService.getNowPlayingMovies(language, page, region).results
+    }
+
+    override suspend fun getTopRatedMovies(
+        language: String,
+        page: Int,
+        region: String?
+    ): List<Movie> {
+        return mainService.getTopRatedMovies(language, page, region).results
+    }
+
+    override suspend fun getUpcomingMovies(
+        language: String,
+        page: Int,
+        region: String?
+    ): List<Movie> {
+        return mainService.getUpcomingMovies(language, page, region).results
+    }
+
+}
--- app/src/main/java/com/salihakbas/movieappcompose/domain/repository/MainRepository.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/domain/repository/MainRepository.kt
@@ -1,3 +1,30 @@
 package com.salihakbas.movieappcompose.domain.repository
 
-interface MainRepository
+import com.salihakbas.movieappcompose.data.model.Movie
+
+interface MainRepository {
+    suspend fun getPopularMovies(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null
+    ): List<Movie>
+
+    suspend fun getNowPlayingMovies(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null
+    ): List<Movie>
+
+    suspend fun getTopRatedMovies(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null
+    ): List<Movie>
+
+    suspend fun getUpcomingMovies(
+        language: String = "en-US",
+        page: Int = 1,
+        region: String? = null
+    ): List<Movie>
+
+}
