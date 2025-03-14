3ba01d761fa9cc94f407f42a13d628da964a69a9
Salih Akbas <salihhakbass@gmail.com>
2025-03-14 21:40:35 +0300

    Added popular, topRated and nowPlaying movies uiState

--- app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeContract.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeContract.kt
@@ -1,9 +1,14 @@
 package com.salihakbas.movieappcompose.ui.home
 
+import com.salihakbas.movieappcompose.data.model.Movie
+
 object HomeContract {
     data class UiState(
         val isLoading: Boolean = false,
         val list: List<String> = emptyList(),
+        val popularMovieList: List<Movie> = emptyList(),
+        val nowPlayingMovieList: List<Movie> = emptyList(),
+        val topRatedMovieList: List<Movie> = emptyList()
     )
 
     sealed class UiAction
