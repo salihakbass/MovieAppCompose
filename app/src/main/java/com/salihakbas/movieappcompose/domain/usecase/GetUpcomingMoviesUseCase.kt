6eb4d8a0974b2a0df35f09ccef8341a2701d44b5
Salih Akbas <salihhakbass@gmail.com>
2025-03-15 02:04:07 +0300

    Added upcoming movie use case

---
+++ app/src/main/java/com/salihakbas/movieappcompose/domain/usecase/GetUpcomingMoviesUseCase.kt
@@ -0,0 +1,18 @@
+package com.salihakbas.movieappcompose.domain.usecase
+
+import com.salihakbas.movieappcompose.data.model.Movie
+import com.salihakbas.movieappcompose.domain.repository.MainRepository
+import javax.inject.Inject
+
+class GetUpcomingMoviesUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(language: String = "en-US", page: Int = 1): Result<List<Movie>> {
+        return try {
+            val movieResponse = repository.getUpcomingMovies(language, page)
+            Result.success(movieResponse)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
