efc71da5a4297f2d557d9191f460985f03ec31db
Salih Akbas <salihhakbass@gmail.com>
2025-03-16 05:13:53 +0300

    Added series functions

--- app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeViewModel.kt
@@ -1,6 +1,14 @@
 package com.salihakbas.movieappcompose.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salihakbas.movieappcompose.domain.usecase.GetAiringTodayTvSeriesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetNowPlayingMoviesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetOnTheAirSeriesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetPopularMoviesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetPopularSeriesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetTopRatedMoviesUseCase
+import com.salihakbas.movieappcompose.domain.usecase.GetUpcomingMoviesUseCase
 import com.salihakbas.movieappcompose.ui.home.HomeContract.UiAction
 import com.salihakbas.movieappcompose.ui.home.HomeContract.UiEffect
 import com.salihakbas.movieappcompose.ui.home.HomeContract.UiState
@@ -12,10 +20,19 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    private val getNowPlayingMoviesUseCase: GetNowPlayingMoviesUseCase,
+    private val getTopRatedMoviesUseCase: GetTopRatedMoviesUseCase,
+    private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
+    private val getAiringTodayTvSeriesUseCase: GetAiringTodayTvSeriesUseCase,
+    private val getOnTheAirSeriesUseCase: GetOnTheAirSeriesUseCase,
+    private val getPopularSeriesUseCase: GetPopularSeriesUseCase
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(UiState())
     val uiState: StateFlow<UiState> = _uiState.asStateFlow()
@@ -23,8 +40,97 @@ class HomeViewModel @Inject constructor() : ViewModel() {
     private val _uiEffect by lazy { Channel<UiEffect>() }
     val uiEffect: Flow<UiEffect> by lazy { _uiEffect.receiveAsFlow() }
 
-    fun onAction(uiAction: UiAction) {
+    init {
+        getPopularMovies()
+        getNowPlayingMovies()
+        getTopRatedMovies()
+        getUpcomingMovies()
+        getAiringTodayTvSeries()
+        getOnTheAirSeries()
+        getPopularSeries()
     }
+
+    fun onAction(uiAction: UiAction) {
+
+    }
+
+    private fun getPopularMovies() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getPopularMoviesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { movies ->
+            updateUiState { copy(popularMovieList = movies) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getNowPlayingMovies() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getNowPlayingMoviesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { movies ->
+            updateUiState { copy(nowPlayingMovieList = movies) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getTopRatedMovies() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getTopRatedMoviesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { movies ->
+            updateUiState { copy(topRatedMovieList = movies) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getUpcomingMovies() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getUpcomingMoviesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { movies ->
+            updateUiState { copy(upcomingMovieList = movies) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getAiringTodayTvSeries() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getAiringTodayTvSeriesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { series ->
+            updateUiState { copy(airingTodayTvSeriesList = series) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getOnTheAirSeries() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getOnTheAirSeriesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { series ->
+            updateUiState { copy(onTheAirSeriesList = series) }
+        }.onFailure {
+
+        }
+    }
+
+    private fun getPopularSeries() = viewModelScope.launch {
+        updateUiState { copy(isLoading = true) }
+        val result = getPopularSeriesUseCase()
+        updateUiState { copy(isLoading = false) }
+        result.onSuccess { series ->
+            updateUiState { copy(popularSeriesList = series) }
+        }.onFailure {
+
+        }
+    }
+
 
     private fun updateUiState(block: UiState.() -> UiState) {
         _uiState.update(block)
