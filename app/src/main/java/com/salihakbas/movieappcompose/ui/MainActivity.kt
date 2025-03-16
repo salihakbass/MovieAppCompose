e5129f9894ea4980802cb7a64066433a96154061
Salih Akbas <salihhakbass@gmail.com>
2025-03-17 02:53:14 +0300

    Added bottomNavigation

--- app/src/main/java/com/salihakbas/movieappcompose/ui/MainActivity.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/ui/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.salihakbas.movieappcompose.navigation.BottomNavigation
 import com.salihakbas.movieappcompose.navigation.NavigationGraph
-import com.salihakbas.movieappcompose.navigation.Screen.Splash
+import com.salihakbas.movieappcompose.navigation.Screen
 import com.salihakbas.movieappcompose.ui.theme.MyappTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,15 +24,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-                    val startDestination = Splash
+                val navController = rememberNavController()
+                val startDestination = Screen.Home
+
+                val currentBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = currentBackStackEntry?.destination?.route
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        if (Screen.showBottomBar(currentDestination)) {
+                            BottomNavigation(navController)
+                        }
+                    }
+                ) { innerPadding ->
+
                     NavigationGraph(
                         navController = navController,
                         startDestination = startDestination,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
+
             }
         }
     }
