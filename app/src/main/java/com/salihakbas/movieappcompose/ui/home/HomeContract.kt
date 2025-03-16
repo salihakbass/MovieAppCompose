ba4a6af5b20f135b2ec16d1839264051a7c0c588
Salih Akbas <salihhakbass@gmail.com>
2025-03-17 02:14:21 +0300

    Added series uiStates

--- app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeContract.kt
+++ app/src/main/java/com/salihakbas/movieappcompose/ui/home/HomeContract.kt
@@ -1,6 +1,7 @@
 package com.salihakbas.movieappcompose.ui.home
 
 import com.salihakbas.movieappcompose.data.model.Movie
+import com.salihakbas.movieappcompose.data.model.Series
 
 object HomeContract {
     data class UiState(
@@ -10,6 +11,10 @@ object HomeContract {
         val nowPlayingMovieList: List<Movie> = emptyList(),
         val topRatedMovieList: List<Movie> = emptyList(),
         val upcomingMovieList: List<Movie> = emptyList(),
+        val airingTodayTvSeriesList: List<Series> = emptyList(),
+        val onTheAirSeriesList: List<Series> = emptyList(),
+        val popularSeriesList: List<Series> = emptyList(),
+        val topRatedSeriesList: List<Series> = emptyList(),
     )
 
     sealed class UiAction
