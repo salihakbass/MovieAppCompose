3bd9fb0359496e53a902f8f841b32eb73d84e8bd
Salih Akbas <salihhakbass@gmail.com>
2025-03-22 03:42:56 +0300

    Add GetMovieCredits use case and MovieCreditsResponse data model

---
+++ app/src/main/java/com/salihakbas/movieappcompose/domain/usecase/movie/GetMovieCreditsUseCase.kt
@@ -0,0 +1,18 @@
+package com.salihakbas.movieappcompose.domain.usecase.movie
+
+import com.salihakbas.movieappcompose.common.Resource
+import com.salihakbas.movieappcompose.data.model.MovieCreditsResponse
+import com.salihakbas.movieappcompose.domain.repository.MainRepository
+import javax.inject.Inject
+
+class GetMovieCreditsUseCase @Inject constructor(
+    private val repository: MainRepository
+) {
+    suspend operator fun invoke(movieId: Int): MovieCreditsResponse {
+        return try {
+            repository.getMovieCredits(movieId)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
